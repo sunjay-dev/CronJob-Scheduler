2cd47e9fc7dd1f24e20d8a38bc947a3ac18a45b5
Sunjay Kumar <ak2604745@gmail.com>
2025-10-10 23:09:14 +0500

    PWA setup completed

--- Frontend/src/pages/Home.tsx
+++ Frontend/src/pages/Home.tsx
@@ -1,6 +1,6 @@
 import { HomeHeader, Hero, Features, Work, OpenSource, HomeFooter } from "../components";
 
-export default function Home() {
+export default function Home() {  
   return (
     <div className="min-h-screen bg-white font-[Inter] selection:bg-purple-400 selection:text-white">
       <HomeHeader home={true} />
@@ -11,5 +11,5 @@ export default function Home() {
       {/* <GettingStart /> */}
       <HomeFooter />
     </div>
-  )
+  );
 }
